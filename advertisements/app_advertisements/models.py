e80216f357f2c5349a5731df2898d584f37fe8a1
Miron135 <136831488+Miron135@users.noreply.github.com>
2023-08-19 16:11:37 +0400

    Add files via upload

---
+++ advertisements/app_advertisements/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib import admin
+class Advertisement(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text="Уместен торг или нет")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'advertisements'
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format.html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
